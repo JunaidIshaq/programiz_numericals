39463d02176db9b7aaddb799f2b834b490c81bbc
junaid <junaid.ishaq@yuzee.com>
2025-11-10 13:14:54 +0500

    Program 1 - Reading Input from Scanner(System.in)

---
+++ java_numericals/src/main/java/com/java/numericals/Program_1.java
@@ -0,0 +1,21 @@
+package com.java.numericals;
+
+
+import java.util.Scanner;
+
+public class Program_1 {
+
+    public static void main(String[] args) {
+        // Creates a reader instance which takes
+        // input from standard input - keyboard
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Please enter a number between 1 and 100");
+
+        // nextInt() reads the next integer from the keyboard
+        int number = reader.nextInt();
+
+        // println() prints the following line to the output screen
+        System.out.println("You entered : " + number);
+    }
+
+}
